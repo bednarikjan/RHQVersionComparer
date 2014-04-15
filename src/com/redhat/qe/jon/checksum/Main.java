53e12ad5e16777d4c2031c870fa3a6a57ebcd52e
Jan Bednarik <jbednari@redhat.com>
2014-04-15 15:31:33 +0200

    now the f the program checks whether the files that have been removed or modified during applying CP on JON are backed up somewhere within given (update) folder

--- src/com/redhat/qe/jon/checksum/Main.java
+++ src/com/redhat/qe/jon/checksum/Main.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
@@ -22,135 +23,138 @@ import java.util.logging.Logger;
 
 public class Main {
 
+    /**
+     * Java program to generate MD5 checksum for files in Java. This Java
+     * example uses core Java security package to generate MD5 checksum for a
+     * File.
+     *
+     *
+     */
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+    private static HashMap<String, String> jonFiles = new HashMap<String, String>();
+    private static HashMap<String, String> updateFiles = new HashMap<String, String>();
+    private static final String fileName = "filesHash.txt";    
 
-   /**
-    * Java program to generate MD5 checksum for files in Java. This Java example
-    * uses core Java security package to generate MD5
-    * checksum for a File.
-    *
-    *
-    */
-   private static final Logger logger = Logger.getLogger(Main.class.getName());
-   private static HashMap<String, String> map  = new HashMap<String, String>();
-   private static final String fileName = "filesHash.txt";
+    private static int chanRemCount = 0;    
 
-   public static void main(String args[]) throws IOException {
-      if(args.length == 0){
-         logger.info("Param: Path to RHQ directory missing");
-         return;
-      }
-      String baseDir = args[0];
-      String ifCompare = null;
-      if(args.length >1){
-         ifCompare = args[1];
-      }
-      getFiles(baseDir);
+    public static void main(String args[]) throws IOException {
+        boolean findBackUp = false;
+        String baseDirJon = new String();
+        String baseDirUpdate = new String();        
+        
+        if (args.length == 0) {
+            logger.info("Param: Path to RHQ directory missing");
+            return;
+        }
 
-      if(ifCompare != null && ifCompare.equals("compare")){
-         HashMap<String, String> hashMap = readMapFromFile();
-         compareMaps(hashMap);
-      }
-      else {
-         writeHashToFile();
-      }
+        baseDirJon = args[0];
 
-   }
-   public static void compareMaps(HashMap<String, String> oldMap){
-      for(Map.Entry<String, String> m: map.entrySet())   {
-         if(oldMap.get(m.getKey()) != null){
-            if(!oldMap.get(m.getKey()).equals(m.getValue())){
-               logger.info("for "+m.getKey()+" hash was "+oldMap.get(m.getKey())+" new hash is "+m.getValue());
+        if (args.length > 1) {
+            findBackUp = true;
+            baseDirUpdate = args[1];
+        }
+
+        getFiles(baseDirJon, jonFiles, (new File(baseDirJon)).getAbsolutePath().length() + 1);
+
+        if (findBackUp) {
+            HashMap<String, String> oldFiles = readMapFromFile();
+            getFiles(baseDirUpdate, updateFiles, (new File(baseDirUpdate)).getAbsolutePath().length() + 1);
+            
+            findBackupFiles(oldFiles);
+
+            System.out.println("Changed/removed files: " + chanRemCount);            
+        } else {
+            writeHashToFile();
+        }
+
+    }
+
+    public static void findBackupFiles(HashMap<String, String> oldMap) {        
+        for (Map.Entry<String, String> old : oldMap.entrySet()) {
+            // file was modified or removed                        
+            if((jonFiles.containsKey(old.getKey()) && !old.getValue().equals(jonFiles.get(old.getKey()))) ||
+               !jonFiles.containsKey(old.getKey())) {              
+                
+                // check if the file is backed up
+                if(!updateFiles.containsKey(old.getKey()) || 
+                   !updateFiles.get(old.getKey()).equals(old.getValue())) {
+                    logger.info("File NOT backed up: " + old.getKey());
+                }
+                
+                chanRemCount++;
+            }
+        }
+
+    }
+
+    public static HashMap<String, String> readMapFromFile() throws IOException {
+        HashMap<String, String> hmap = new HashMap<String, String>();
+        FileReader fr = new FileReader(fileName);
+        BufferedReader br = new BufferedReader(fr);
+        try {
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                String[] str = line.split(",");
+                hmap.put(str[0], str[1]);
+            }
+        } finally {
+            br.close();
+            fr.close();
+        }
+        return hmap;
+    }
+
+    public static void writeHashToFile() throws IOException {
+        FileWriter fw = new FileWriter(fileName);
+        try {
+            for (Map.Entry<String, String> m : jonFiles.entrySet()) {
+                fw.write(m.getKey() + "," + m.getValue() + "\n");
+            }
+        } finally {
+            fw.close();
+        }
+
+    }   
+    
+    public static void getFiles(String baseDir, HashMap<String, String> map, int pathToRemove) {
+
+        File file = new File(baseDir);
+        File[] files = file.listFiles();
+
+        for (File f : files) {
+            if (f.isDirectory()) {
+                getFiles(f.getAbsolutePath(), map, pathToRemove);
+            } else {
+                map.put(f.getAbsolutePath().substring(pathToRemove), checkSum(f.getAbsolutePath()));
 
             }
-            oldMap.remove(m.getKey());
-         }
-         else {
-            logger.info("New file added "+m.getKey());
-         }
+        }
 
-      }
-      if(oldMap.size() > 0){
-          for (Map.Entry<String, String> m: oldMap.entrySet())
-          {
-             logger.info(m.getKey()+" file has been removed");
-          }
+    }
+    /*
+     * Calculate checksum of a File using MD5 algorithm
+     */
 
-      }
+    public static String checkSum(String path) {
+        String checksum = null;
+        try {
+            FileInputStream fis = new FileInputStream(path);
+            MessageDigest md = MessageDigest.getInstance("MD5");
 
-   }
-   public static HashMap<String, String> readMapFromFile() throws IOException {
-      HashMap<String, String> hmap = new HashMap<String, String>();
-      FileReader fr = new FileReader(fileName);
-      BufferedReader br = new BufferedReader(fr);
-      try{
-      String line = null;
-      while ((line=br.readLine())!=null)
-      {
-         String [] str = line.split(",");
-         hmap.put(str[0],str[1]);
-      }
-      }
-      finally{
-         br.close();
-         fr.close();
-      }
-      return hmap;
-   }
+            //Using MessageDigest update() method to provide input
+            byte[] buffer = new byte[8192];
+            int numOfBytesRead;
+            while ((numOfBytesRead = fis.read(buffer)) > 0) {
+                md.update(buffer, 0, numOfBytesRead);
+            }
+            byte[] hash = md.digest();
+            checksum = new BigInteger(1, hash).toString(16); //don't use this, truncates leading zero
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        } catch (NoSuchAlgorithmException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
 
-   public static void  writeHashToFile() throws IOException {
-      FileWriter fw = new FileWriter(fileName);
-      try{
-         for (Map.Entry<String, String> m: map.entrySet())   {
-            fw.write(m.getKey() + "," + m.getValue()+"\n");
-         }
-      }
-      finally{
-         fw.close();
-      }
-
-   }
-   public static void getFiles(String baseDir){
-
-      File file =  new File(baseDir);
-      File [] files = file.listFiles();
-
-      for (File f : files){
-         if (f.isDirectory()){
-            getFiles(f.getAbsolutePath()) ;
-         }
-         else {
-            map.put(f.getAbsolutePath(), checkSum(f.getAbsolutePath()));
-
-         }
-      }
-
-   }
-   /*
-    * Calculate checksum of a File using MD5 algorithm
-    */
-   public static String checkSum(String path){
-      String checksum = null;
-      try {
-         FileInputStream fis = new FileInputStream(path);
-         MessageDigest md = MessageDigest.getInstance("MD5");
-
-         //Using MessageDigest update() method to provide input
-         byte[] buffer = new byte[8192];
-         int numOfBytesRead;
-         while( (numOfBytesRead = fis.read(buffer)) > 0){
-            md.update(buffer, 0, numOfBytesRead);
-         }
-         byte[] hash = md.digest();
-         checksum = new BigInteger(1, hash).toString(16); //don't use this, truncates leading zero
-      } catch (IOException ex) {
-         logger.log(Level.SEVERE, null, ex);
-      } catch (NoSuchAlgorithmException ex) {
-         logger.log(Level.SEVERE, null, ex);
-      }
-
-      return checksum;
-   }
-
-
+        return checksum;
+    }
 }
-
